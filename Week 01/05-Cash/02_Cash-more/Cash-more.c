c32775004627d74ac13e9e41f0c1017dec3c5917
Mono_sann <115377829+momo009l@users.noreply.github.com>
2026-02-10 20:55:48 +0700

    Update Cash-more.c

--- Week 01/05-Cash/02_Cash-more/Cash-more.c
+++ Week 01/05-Cash/02_Cash-more/Cash-more.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(void)
+{
+    float dollars;
+
+    do
+    {
+        printf("Change owed: ");
+        scanf("%f", &dollars);
+    }
+    while (dollars <= 0);
+
+    int cents = round(dollars * 100);
+
+    int n;
+    printf("Number of coin types: ");
+    scanf("%d", &n);
+
+    int coins[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Coin %d value (cents): ", i + 1);
+        scanf("%d", &coins[i]);
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        count += cents / coins[i];
+        cents %= coins[i];
+    }
+
+    printf("Total coins: %d\n", count);
+}
