27f907cdf56f8dc6d92c3729b2b505692901853d
Mono_sann <115377829+momo009l@users.noreply.github.com>
2026-02-10 19:40:45 +0700

    Update mario-less.c

--- Week 01/04-Mario/01_Mario-less/mario-less.c
+++ Week 01/04-Mario/01_Mario-less/mario-less.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int n;
+
+    do
+    {
+        printf("Height: ");
+        scanf("%d" , &n);
+    }
+    while (n < 1 || n > 8);
+
+    for (int i = 1; i <= n; i++)
+    {
+         for (int j = 0; j < i; j++)
+         {
+             printf("#");
+         }
+         printf("\n");
+    }
+}
