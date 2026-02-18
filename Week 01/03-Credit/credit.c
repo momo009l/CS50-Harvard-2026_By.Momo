f44b473e28f3b46ba40b3bdae6e9bd40e20f4530
Mono_sann <115377829+momo009l@users.noreply.github.com>
2026-02-18 16:07:41 +0700

    Refactor credit.c for coin calculation and input

--- Week 01/03-Credit/credit.c
+++ Week 01/03-Credit/credit.c
@@ -1,62 +1,28 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    long number = get_long("Number: ");
+    int cents;
 
-    int sum = 0;
-    long n = number;
-    int position = 0;
-
-    while (n > 0)
+    do
     {
-        int digit = n % 10;
+        printf("Change owed: ");
+        scanf("%d", &cents);
+    }
+    while (cents < 0);
 
-        if (position % 2 == 1)
-        {
-            int doubled = digit * 2;
-            sum += doubled / 10 + doubled % 10;
-        }
-        else
-        {
-            sum += digit;
-        }
+    int coins = 0;
 
-        n /= 10;
-        position++;
-    }
+    coins += cents / 25;
+    cents %= 25;
 
-    if (sum % 10 != 0)
-    {
-        printf("INVALID\n");
-        return 0;
-    }
+    coins += cents / 10;
+    cents %= 10;
 
-    int length = 0;
-    long start = number;
+    coins += cents / 5;
+    cents %= 5;
 
-    while (start >= 100)
-    {
-        start /= 10;
-        length++;
-    }
-    length += 2;
+    coins += cents;
 
-    if (length == 15 && (start == 34 || start == 37))
-    {
-        printf("AMEX\n");
-    }
-    else if (length == 16 && (start >= 51 && start <= 55))
-    {
-        printf("MASTERCARD\n");
-    }
-    else if ((length == 13 || length == 16) && (start / 10 == 4))
-    {
-        printf("VISA\n");
-    }
-    else
-    {
-        printf("INVALID\n");
-    }
+    printf("%d\n", coins);
 }
