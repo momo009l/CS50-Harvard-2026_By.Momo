55d1242ad0c8c985e2bd71dfee257616c56c370f
Mono_sann <115377829+momo009l@users.noreply.github.com>
2026-02-18 16:05:29 +0700

    Refactor name input and greeting using CS50 library

--- Week 01/02-Hello,It’s Me./Me.c
+++ Week 01/02-Hello,It’s Me./Me.c
@@ -1,10 +1,8 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-  char name[100];
-  
-  printf("What is your name?\n");
-  scanf("%s" , name);
-  printf("Hello, %s\n", name);
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
 }
